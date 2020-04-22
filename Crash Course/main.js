4890b03eb9c00c15e00bf423e4efd2455e8e3326
ClaudioDroid <claudio.v.lima2@gmail.com>
2020-04-22 14:18:41 -0300

    JS Crash course

--- Crash Course/main.js
+++ Crash Course/main.js
@@ -1,16 +1,41 @@
-alert('teste');
+/* -----  ----- */
+//alert('Notification test');
 
-console.error('errru');
-
-
-
-
-
+console.log('Hello world');
+console.error('This is an error');
+console.warn('This is an warning');
 
 
+/* ----- let, const ----- */
+let age = 30;
+const name = 'Juao';
+
+document.write('Hi, ' + name + ', you are ' + age + ' years old.');
 
 
+/* ----- String, Numbers, Boolean, null, undefined ----- */
+const yourName = 'Creuza';
+const yourAge = 28;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
 
+console.log( typeof yourAge );
+
+
+/* ----- Concatenation ----- */
+console.log('Your name is ' + yourName + ' and you are ' + age);
+console.log(`My name is ${name} and I am ${age}`)
+
+
+/* -----  ----- */
+const s = 'Hello world';
+
+console.log(s.length);
+console.log(s.substring(0, 4).toUpperCase());
+console.log( s.split('') );
 
 
 
