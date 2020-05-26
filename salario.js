36ae6d12266d68f64279eaefc32da34710365ae4
ClaudioDroid <claudio.v.lima2@gmail.com>
2020-05-26 15:02:41 -0300

    js practice

---
+++ salario.js
@@ -0,0 +1,15 @@
+let salario = parseFloat( window.prompt ("Digite o valor do salário: "));
+
+let reajuste = parseFloat(window.prompt ("Digite o percentual de reajuste do salario:"));
+
+let novoSalario = salario + (reajuste * salario) / 100;
+
+console.log ("O novo salário é: " + novoSalario);
+
+
+
+
+
+
+
+
